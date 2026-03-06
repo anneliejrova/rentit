251b491ac618c2f7f5d1bb58e2efb8d5d3c0c72e
Annelie <annelie.j.rova@gmail.com>
2026-03-06 11:16:50 +0100

    router code comments added
    Co-authored-by: Rebecca Martis

--- src/router.js
+++ src/router.js
@@ -1,21 +1,25 @@
 import { routes } from './routes.js';
 
+//gets slug, no return = DOM, loads view when called
 function loadView(slug) {
   const route = routes.find(r => r.slug === slug);
 
   if (!route) return;
 
+  //calls component to load the correct view
   route.component().then(module => {
     document.querySelector('main').innerHTML = module.render();
     document.title = route.title + ' | Rentit';
   });
 }
 
+//gets slug, no return = DOM, adds post to browser history and uppdates URL, calls for loadView
 export function navigate(slug) {
   history.pushState(null, '', '#' + slug);
   loadView(slug);
 }
 
+//listens to URL-hash changes, makes back- and forwardbutton work
 window.addEventListener('hashchange', () => {
   loadView(location.hash.slice(1) || 'home');
 });
