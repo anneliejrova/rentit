e6ca7313a13b68645905b4de80f2fa3cc85b5524
Annelie <annelie.j.rova@gmail.com>
2026-03-09 14:43:44 +0100

    updated home view

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,7 @@
-
-
-export function render() {
+export function render(route) {
   return `
     <section aria-labelledby="home-heading">
       <h2 id="home-heading">Rentit huvudsida</h2>
-      
-      </nav>
     </section>
   `;
 }
