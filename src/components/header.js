4de91830f774fd1742c55161271165b16eab50fe
Annelie <annelie.j.rova@gmail.com>
2026-03-13 11:05:31 +0100

    quick fix -header

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ export function renderHeader(route) {
   const header = document.querySelector("header");
 
   header.innerHTML = /* html */ `
-  <div class="shadow-md"> <!-- header wrapper -->
+  <div class="shadow-md  bg-white"> <!-- header wrapper -->
     <div class="flex justify-between "> 
       <div class="bg-white px-6 py-4 flex content-center"> <!-- logo + category titel (home = slogan) -->
         <a href="/">
