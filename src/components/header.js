ff09af4b3cdbc6dd37a3e1d524af333ad8dec3fb
Annelie <annelie.j.rova@gmail.com>
2026-03-14 20:33:13 +0100

    Added category photos and moved img to public

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ export function renderHeader(route) {
     <div class="flex justify-between "> 
       <div class="bg-white px-6 py-4 flex content-center"> <!-- logo + category titel (home = slogan) -->
         <a href="/">
-          <img src=../img/logo/logo.png class="w-50 h-auto" alt="Rentit">
+          <img src=../public/img/logo/logo.png class="w-50 h-auto" alt="Rentit">
         </a>
         <h1 class="content-center text-2xl md:text-4xl font-bold text-gray-800 ml-6">${route.headertxt}</h1>
       </div>
