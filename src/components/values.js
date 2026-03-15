160f4fb6af4c28c38e01ef08338d67c6edeffac0
Beckasinis <beccis22@hotmail.com>
2026-03-15 21:39:17 +0100

    image text and button in a hero, also started to create som values underneath with icons

---
+++ src/components/values.js
@@ -0,0 +1,24 @@
+
+export function renderValues() {
+  return /*html*/`
+
+<section class="relative min-h-48">
+  <div class="relative z-10 flex items-center justify-center gap-16 py-16">
+    <div class="w-75 mt-16 mb-16">
+      <img src="./img/icons/icons1.png" alt=""/>
+      <p>Miljö & Hållbarhet</p>
+    </div>
+
+    <div class="w-75 mt-16 mb-16">
+      <img src="./img/icons/icons2.png" alt=""/>
+      <p>Hyr instället för att köpa</p>
+    </div>
+
+    <div class="w-25 mt-16 mb-16">
+      <img src="./img/icons/icons3.png" alt=""/>
+      <p>Spara plats i förrådet<p>
+    </div>
+  </div>
+</section>
+  `;
+}
