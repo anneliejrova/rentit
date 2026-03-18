6088d377d2bfeacb07546582c8853693ea27d6fc
Annelie <annelie.j.rova@gmail.com>
2026-03-18 15:59:08 +0100

    quick fix about

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ export function renderAbout() {
       <h2 class="text-3xl md:text-4xl font-bold font-serif">Om oss</h2>
       <p class="text-2xl">
         <p class="text-xl">Rentit – Varför äga när du kan göra?</p>
-        <p class="text-xl">Vi tror på projektet, inte på prylarna.</p>
+        <p class="text-xl">Tro på projektet, inte på prylarna!</p>
         <p class="text-lg">Rentit är din lokala partner för allt det där du behöver – men bara ibland. Genom att hyra cirkulärt minskar vi tillsammans både svinn och onödiga inköp. Oavsett om du ska rädda skörden med en saftmaja, klyva vinterveden eller fixa altanen med proffsverktyg, ser vi till att du har rätt grejer på plats. Alltid servat, alltid hållbart, upphämtning eller direkt till din dörr.</p>
       </p>
     </div>
