c2b4f0711f3c70b4d83907781a3af562383245f5
Annelie <annelie.j.rova@gmail.com>
2026-03-20 15:41:27 +0100

    fixed category responsive

--- src/views/category.js
+++ src/views/category.js
@@ -17,13 +17,13 @@ export async function render(route) {
 
   return /* html */ `
  
-   <div class="p-6 md:p-17 bg-(--accent) rounded-2xl mb-8 flex flex-col md:flex-row gap-6 md:gap-8 mx-auto items-center w-[95vw] md:w-[60vw] shadow-2xl">
+   <div class="p-6 md:p-17 bg-(--accent) rounded-2xl mb-8 flex flex-col lg:flex-row gap-6 md:gap-8 mx-auto items-center w-[95vw] md:w-[60vw] shadow-2xl">
     ${category.imgUrl ? `
-        <img src="${category.imgUrl}" alt="${category.name}" class="w-full md:w-64 h-48 md:h-66 object-cover rounded-xl shrink-0 shadow-md">
+        <img src="${category.imgUrl}" alt="${category.name}" class="w-full lg:w-64 h-48 md:h-66 object-cover rounded-xl shrink-0 shadow-md">
     ` : ""}
     <div class="flex flex-col gap-3">
-        <h1 class="text-2xl md:text-4xl font-bold" style="color: var(--accent-light)">${category.descriptionTitle}</h1>
-        <p class="text-sm md:text-1xl leading-relaxed max-w-prose">${category.description}</p>
+        <h1 class="text-2xl lg:text-4xl font-bold" style="color: var(--accent-light)">${category.descriptionTitle}</h1>
+        <p class="text-sm lg:text-1xl leading-relaxed max-w-prose">${category.description}</p>
     </div>
 </div>
 
