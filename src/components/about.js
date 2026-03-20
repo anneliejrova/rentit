7c415a864f76c9dc771b9e549ae912a53a0dd63c
Annelie <annelie.j.rova@gmail.com>
2026-03-20 02:13:20 +0100

    Fixed checkout issues and responsivity

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,13 @@
 export function renderAbout() {
   return /*html*/`
   <div class="pt-15 bg-olive-200">
-    <div class="flex flex-col max-w-1/2 mx-auto gap-6">
-      <h2 class="text-3xl md:text-4xl font-bold font-serif">Om oss</h2>
-      <p class="text-2xl">
-        <p class="text-xl">Rentit – Varför äga när du kan hyra?</p>
-        <p class="text-xl">Äg projektet, inte prylarna!</p>
-        <p class="text-xl">Rentit är din lokala partner för allt det där du behöver – men bara ibland. Genom att hyra cirkulärt minskar vi tillsammans både svinn och onödiga inköp. Oavsett om du ska rädda skörden med en saftmaja, klyva vinterveden eller fixa altanen med proffsverktyg, ser vi till att du har rätt grejer på plats. <p>
-        <p class="text-xl">Alltid servat, alltid hållbart, upphämtning eller direkt till din dörr.</p>
+    <div class="flex flex-col max-w-3/4 md:max-w-1/2 mx-auto gap-3 md:gap-6">
+      <h2 class="text-xl md:text-4xl font-bold font-serif">Om oss</h2>
+      <p class="md:text-2xl">
+        <p class="md:text-xl">Rentit – Varför äga när du kan hyra?</p>
+        <p class="md:text-xl">Äg projektet, inte prylarna!</p>
+        <p class="md:text-xl">Rentit är din lokala partner för allt det där du behöver – men bara ibland. Genom att hyra cirkulärt minskar vi tillsammans både svinn och onödiga inköp. Oavsett om du ska rädda skörden med en saftmaja, klyva vinterveden eller fixa altanen med proffsverktyg, ser vi till att du har rätt grejer på plats. <p>
+        <p class="md:text-xl">Alltid servat, alltid hållbart, upphämtning eller direkt till din dörr.</p>
       </p>
     </div>
     </div>
