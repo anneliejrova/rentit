eae047b605931884e0adca2b7e93483e53bba33d
Beckasinis <beccis22@hotmail.com>
2026-03-20 15:41:38 +0100

    fix responsive hamburger

--- src/components/mobileMenu.js
+++ src/components/mobileMenu.js
@@ -13,10 +13,10 @@ export function renderMobileMenu() {
   );
 
   return /*html*/`
-    <div id="mobileMenu" class="hidden md:hidden absolute right-0 max-w-lg bg-white shadow-lg p-4 flex-col gap-4 z-50">
+    <div id="mobileMenu" class="hidden lg:hidden absolute right-0 max-w-lg bg-white shadow-lg p-4 flex-col gap-4 z-50">
 
       <!-- search - (hidden on desktop) -->
-      <div class="block md:hidden">
+      <div class="block lg:hidden">
         ${renderSearch('-mobile', 'right')}
       </div>
       <nav class="flex flex-col gap-2">
